502654d8673e3d32d5f22976d5a121500d3f95fc
Kra1o5 <kra1o5x@gmail.com>
2022-08-22 20:22:04 +0000

    overlay: Introduce Immersive Navigation [3/3]
    Based in Hide gesture navbar from Syberia project
    
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: Víctor Santoja <victorsantoja@gmail.com>
    Change-Id: I805df9d44920b0274f8495820e401cb1bac1e16b

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -69,7 +69,8 @@ PRODUCT_PACKAGES += \
     IconShapeTaperedRectOverlay \
     IconShapeTeardropOverlay \
     IconShapeVesselOverlay \
-    ParanoidLauncherOverlay
+    ParanoidLauncherOverlay \
+    ImmersiveNavigationOverlay
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aospa/overlay/static
 PRODUCT_PACKAGE_OVERLAYS += vendor/aospa/overlay/static
